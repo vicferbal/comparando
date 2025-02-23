95baf0907841c14d30ea3a9bac1be59f5477e764
vicferbal <victorferrandez94@gmail.com>
2025-02-23 14:44:49 +0100

    Ejercicio 9. Probando comparadores debiles == y fuertes ===.

---
+++ src/script.js
@@ -0,0 +1,13 @@
+// comprobando las comparaciones imprimiendolas y viendolas en console en el navegador
+console.log("\"5\" == 5:", "5" == 5);
+console.log("\"5\" === 5:", "5" === 5);
+console.log("0 == false:", 0 == false);
+console.log("0 === false:", 0 === false);
+console.log("null == undefined:", null == undefined);
+console.log("null === undefined:", null === undefined);
+console.log("true == 1:", true == 1);
+console.log("true === 1:", true === 1);
+console.log("\"\" == 0:", "" == 0);
+console.log("\"\" === 0:", "" === 0);
+console.log("\"0\" == false:", "0" == false);
+console.log("\"0\" === false:", "0" === false);
